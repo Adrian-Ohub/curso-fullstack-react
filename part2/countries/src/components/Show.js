042a802053b8a4287772f7d368c65d9c7c1542cf
Adrian-Ohub <63118713+Adrian-Ohub@users.noreply.github.com>
2021-03-05 22:12:42 +0100

    2.14 data countries with .env (dont push)

--- part2/countries/src/components/Show.js
+++ part2/countries/src/components/Show.js
@@ -1,4 +1,3 @@
 export const Show = ({ handlerShow, countrySelected }) => {
-  console.log(handlerShow, countrySelected);
   return <button onClick={() => handlerShow(countrySelected)}>show</button>;
 };
